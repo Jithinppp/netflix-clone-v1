3b584e44377031d1b2c16c129afbddc6f9d32101
Jithinppp <jithinp968@gmail.com>
2023-02-13 01:27:54 +0530

    env changes

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -7,12 +7,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyALci28M5GIfe73gxlpgLE8aM7eWeeb_SE",
-  authDomain: "netflix-clone-v1-a7180.firebaseapp.com",
-  projectId: "netflix-clone-v1-a7180",
-  storageBucket: "netflix-clone-v1-a7180.appspot.com",
-  messagingSenderId: "466304164019",
-  appId: "1:466304164019:web:169573ceb9eb08b42fd0c6",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
